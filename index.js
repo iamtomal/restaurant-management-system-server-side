733a9187bf233df9dd1091453fc553d23de60bee
iamtomal <developer.tomal@gmail.com>
2023-11-09 16:26:11 +0600

    create food details section and deploy project

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
       });
 
 
+      app.get("/food/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await foodCollection.findOne(query);
+        res.send(result);
+      });
+
+
     app.post("/food", async (req, res) => {
         const newFood = req.body;
         console.log(newFood);
